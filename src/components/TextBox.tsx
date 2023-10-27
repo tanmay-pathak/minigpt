95327ae8bdc5eed44701c198c087f43961bd23a0
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2023-10-26 19:30:40 -0600

    styled the textbox

--- src/components/TextBox.tsx
+++ src/components/TextBox.tsx
@@ -9,7 +9,9 @@ const TextBox = ({ onSubmit }: TextBoxProps) => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    onSubmit(text)
+    if (text) {
+      onSubmit(text)
+    }
     setText("")
   }
 
@@ -18,9 +20,19 @@ const TextBox = ({ onSubmit }: TextBoxProps) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={text} onChange={handleChange} />
-      <button type="submit">Send</button>
+    <form onSubmit={handleSubmit} className="fixed bottom-0 flex w-full p-6">
+      <input
+        type="text"
+        value={text}
+        onChange={handleChange}
+        className="w-full rounded-xl"
+      />
+      <button
+        type="submit"
+        className="ml-3 rounded-xl bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+      >
+        Send
+      </button>
     </form>
   )
 }
