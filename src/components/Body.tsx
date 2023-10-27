864289aaa79f2cb7a37ede1aee443a9c37200d64
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2023-10-26 19:04:18 -0600

    added basic chat bubble

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,10 +1,27 @@
 import React from "react"
 import { useLocalStorage } from "@uidotdev/usehooks"
+import ChatBubble from "@components/ChatBubble"
 
 const Body = () => {
   const [conversation, setConversation] = useLocalStorage("conversation", [])
 
-  return <div className="m-1 text-center">Hey</div>
+  const messages = [
+    { message: "Hi", isUser: false },
+    { message: "Hello", isUser: true },
+  ]
+
+  return (
+    <div className="m-1 text-center">
+      {messages.map((msg, i) => {
+        const className = msg.isUser ? "flex flex-row-reverse" : "flex"
+        return (
+          <div className={className} key={i}>
+            <ChatBubble message={msg.message} isUser={msg.isUser} />
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Body
