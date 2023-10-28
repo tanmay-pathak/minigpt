7ae09df8ae1a51a132ae35e359695a6c95722a82
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2023-10-27 23:21:11 -0600

    added a welcome component

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,16 @@
+const Welcome = () => {
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center">
+        <ul className="grid gap-10 grid-cols-2">
+          <li>Minimalistic UI</li>
+          <li>Local only</li>
+          <li>Fast</li>
+          <li>Simple</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Welcome
