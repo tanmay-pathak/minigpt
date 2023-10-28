cc36a9d3b39e14f3bfd5c4ddfd6b9f69c97cdf51
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2023-10-27 22:51:38 -0600

    added progressive web app configs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,6 +20,82 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
+          <meta name="application-name" content="MiniGPT" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="MiniGPT" />
+          <meta name="description" content="A minimalistic ChatGPT UI." />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta
+            name="msapplication-config"
+            content="/icons/browserconfig.xml"
+          />
+          <meta name="msapplication-TileColor" content="#00A67E" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#00A67E" />
+
+          <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/icons/touch-icon-ipad.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/touch-icon-iphone-retina.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="167x167"
+            href="/icons/touch-icon-ipad-retina.png"
+          />
+
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            rel="mask-icon"
+            href="/icons/safari-pinned-tab.svg"
+            color="#00A67E"
+          />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="https://minigpt.pages.dev" />
+          <meta name="twitter:title" content="MiniGPT" />
+          <meta
+            name="twitter:description"
+            content="A minimalistic ChatGPT UI."
+          />
+          <meta name="twitter:creator" content="@tanmay_pathak" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="MiniGPT" />
+          <meta
+            property="og:description"
+            content="A minimalistic ChatGPT UI."
+          />
+          <meta property="og:site_name" content="MiniGPT" />
+          <meta property="og:url" content="https://minigpt.pages.dev" />
+          <meta property="og:image" content="/public/icon-512x512.png" />
         </Head>
         <body>
           <Main />
